ae1537d6b84859c778f11c15ad24612ec5ecfbab
MishMick <mishallovic@gmail.com>
2015-09-05 11:17:33 +0530

    random

---
+++ Assets/Sounds/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+    static MusicPlayer instance = null;
+	// Use this for initialization
+	void Start () {
+	if(instance!=null)
+	{
+	 Destroy (gameObject);
+	 print ("Deplicate music player self destruction");
+	}
+	else{
+	GameObject.DontDestroyOnLoad(gameObject);
+	}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
